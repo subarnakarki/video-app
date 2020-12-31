006701b3622517adc398225685118cf5021dd045
subarna karki <smukhiyabe@gannett.com>
2020-12-31 11:34:23 -0500

    Create video item component

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import VideoItem from './VideoItem'
 
 const VideoList = ({videos}) => {
+    const renderedList = videos.map((video) => {
+        return <VideoItem video={video}/>
+    });
     return (
         <div>
-            {videos.length}
+            {renderedList}
         </div>
     )
 }
