9cef6dd35efd28c5d60224a611282f0138c53e32
subarna karki <smukhiyabe@gannett.com>
2020-12-31 12:02:20 -0500

    Update video item component and add styles

--- src/components/VideoItem.jsx
+++ src/components/VideoItem.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const VideoItem = ({video}) => {
+    const {snippet} = video;
+    const {title, thumbnails, description} = snippet;
     return (
-        <div>
-        {video.snippet.title}
+        <div className="item">
+        <img className="ui image" src={`${thumbnails.medium.url}`} alt={`${description}`}/>
+        <div className="content">
+            <div className="header">{title}</div>
+        </div>
         </div>
     )
 };
